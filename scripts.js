470bdf0f059ada02f737600ce0975fd79f8e4678
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-08-13 16:17:21 +0530

    scripts added to collapse

--- scripts.js
+++ scripts.js
@@ -1,31 +1,39 @@
-const collapsibleButton = document.querySelector('.collapsible');
-const content = document.querySelector('.content');
-
-collapsibleButton.addEventListener('click', function() {
-    content.classList.toggle('active');
-    if (content.style.display === 'block') {
-        content.style.display = 'none';
-    } else {
-        content.style.display = 'block';
-    }
-});
-
-
 
 
 function dropDownHandler(buttonID) {
     const favButton = document.getElementById(buttonID);
     const icon = favButton.querySelector('i');
 
-    
         if (icon.classList.contains('fa-plus')) {
             icon.classList.remove('fa-plus');
             icon.classList.add('fa-minus');
-            
+
+        // adding collapse features
+             
+
         } else {
             icon.classList.remove('fa-minus');
             icon.classList.add('fa-plus');
         }
+
+
+
+
+        
+        const cardContents = document.getElementById(favButton.id+'-Contents');
+    
+        favButton.addEventListener('click', function() {
+            cardContents.classList.toggle('active');
+            if (cardContents.style.display === 'block') {
+                cardContents.style.display = 'none';
+            } else {
+                cardContents.style.display = 'block';
+            }
+        });
+
+
+
+
     };
     
 
